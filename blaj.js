50403849c46bba3eb58c0ae624bf409aa503efad
David Keijser <keijser@gmail.com>
2015-07-29 11:24:11 +0200

    replace custom trig functions with gl-vec2

--- blaj.js
+++ blaj.js
@@ -6,9 +6,13 @@ setTimeout(function () {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.translate(200, 200)
-  arrow(ctx, [ {x: 0, y: 0}
-             , {x: 200, y: 0}
-             , {x: 250, y: 100}
-             , {x: 250, y: 200}
+
+  ctx.lineWidth = 2
+  ctx.strokeStyle = 'red'
+
+  arrow(ctx, [ [0, 0]
+             , [200, 0]
+             , [250, 100]
+             , [250, 200]
              ])
 }, 400)
