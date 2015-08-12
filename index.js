be7575e9f74b52ffc616161a861e9874764d2e05
David Keijser <keijser@gmail.com>
2015-08-12 11:01:30 +0200

    include redux-devtools

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import Editor from './lib/editor'
+import { devTools } from 'redux-devtools'
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'
 import { reduce } from './lib/map'
 
-let store = applyMiddleware(thunk)(createStore)(reduce)
+let store = applyMiddleware(thunk)(devTools()(createStore))(reduce)
 
-React.render(React.createElement(Provider, {store},
-                                 () => React.createElement(Editor)),
-             document.getElementById('app'))
+React.render(
+  React.createElement(
+    'div', null,
+    React.createElement(Provider, {store},
+                        () => React.createElement(Editor)),
+    React.createElement(DebugPanel, {top: true, right: true, bottom: true},
+                        React.createElement(DevTools, {store, monitor: LogMonitor})),
+  ),
+  document.getElementById('app'))
