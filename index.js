b1a5dde113e72f336016386f2f851f4d6eebe541
David Keijser <keijser@gmail.com>
2015-09-13 22:26:47 +0200

    move master reducer to reducers package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'babel-core/lib/polyfill'
 
 import React from 'react'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { devTools } from 'redux-devtools'
@@ -9,14 +9,9 @@ import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'
 
 import Editor from './lib/containers/editor'
 import storage, { storedState } from './lib/middlewares/storage'
-import color from './lib/reducers/color'
-import tiles from './lib/reducers/tiles'
+import reducer from './lib/reducers'
 
 const init = storedState()
-const reducer = combineReducers({
-  color,
-  tiles
-})
 const store = applyMiddleware(thunk, storage)(devTools()(createStore))(reducer, init)
 
 React.render(
